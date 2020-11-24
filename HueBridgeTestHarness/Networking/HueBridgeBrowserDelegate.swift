24e7a34e66c9bf470b722ba46bba7f5724ce0d1e
jprusso224 <jprusso224@users.noreply.github.com>
2020-11-23 19:16:23 -0500

    Prevent the NetService browser delegate from appending duplicate services.

--- HueBridgeTestHarness/Networking/HueBridgeBrowserDelegate.swift
+++ HueBridgeTestHarness/Networking/HueBridgeBrowserDelegate.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+//Utility struct to store the name and pair the service to the delegate.
 struct DiscoveredService
 {
     var name: String
@@ -18,24 +19,41 @@ class HueBridgeBrowserDelegate : NSObject, NetServiceBrowserDelegate, Observable
 {
     var DEFAULT_ADDRESS_RESOLUTION_TIMEOUT_SEC: TimeInterval = 5
     
+    //publish the list of discovered so listening views get updated with the service details.
     @Published var discoveredServices: [DiscoveredService] = []
     
+    //Keep a cache of discovered service names for quick look-up.
+    private var discoveredServiceNames: [String] = []
+    
+    //Eventually this will be set by the user.
     private var targetHueBridgeServiceName: String = ""
     
     //Callback provided by NetServiceBrowserDelegate
     func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool)
     {
-        //Create a new service delegate.
-        let delegate = HueBridgeServiceDelegate()
-        
-        //Assign the delegate to the service.
-        service.delegate = delegate
-        
-        //Command the service to resolve the address.
-        service.resolve(withTimeout: DEFAULT_ADDRESS_RESOLUTION_TIMEOUT_SEC)
-        
-        //Append a DiscoveredService object with a name, service, and delegate.
-        discoveredServices.append(DiscoveredService(name: service.name, service: service, delegate: delegate))
+        //Only process the callback for new services, just in case.
+        if(!discoveredServiceNames.contains(service.name))
+        {
+            //Create a new service delegate.
+            let delegate = HueBridgeServiceDelegate()
+            
+            //Assign the delegate to the service.
+            service.delegate = delegate
+            
+            //Command the service to resolve the address.
+            service.resolve(withTimeout: DEFAULT_ADDRESS_RESOLUTION_TIMEOUT_SEC)
+            
+            //Append a DiscoveredService object with a name, service, and delegate.
+            discoveredServices.append(DiscoveredService(name: service.name, service: service, delegate: delegate))
+            discoveredServiceNames.append(service.name) //Also append the name to the name cache.
+        }
+    }
+    
+    //Callback provided by NetServiceBrowserDelegate
+    func netServiceBrowser(_ browser: NetServiceBrowser, didRemove service: NetService, moreComing: Bool)
+    {
+        //TODO: Do more than just print to the console.
+        print("Service removed: \(service.name)")
     }
     
     //The user should call this to set the target.
@@ -45,15 +63,11 @@ class HueBridgeBrowserDelegate : NSObject, NetServiceBrowserDelegate, Observable
     {
         var set: Bool = false
         
-        for discoveredService in discoveredServices
+        //Check if service name was found (is valid)
+        if(discoveredServiceNames.contains(serviceName))
         {
-            //Check if service name was found (is valid)
-            if(targetHueBridgeServiceName == discoveredService.name)
-            {
-                targetHueBridgeServiceName = serviceName
-                set = true
-                break //Exit the loop once a valid name is found
-            }
+            targetHueBridgeServiceName = serviceName
+            set = true
         }
         
         return set
@@ -63,6 +77,7 @@ class HueBridgeBrowserDelegate : NSObject, NetServiceBrowserDelegate, Observable
     func reset()
     {
         discoveredServices = []
+        discoveredServiceNames = []
         targetHueBridgeServiceName = ""
     }
     
